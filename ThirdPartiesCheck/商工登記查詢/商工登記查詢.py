b66b8a90faa54342a71af9e4e4c607844dba2196
Jiann <72090089+Jiann-Ma@users.noreply.github.com>
2021-12-16 16:47:08 +0800

    Update 商工登記查詢.py

--- ThirdPartiesCheck/商工登記查詢/商工登記查詢.py
+++ ThirdPartiesCheck/商工登記查詢/商工登記查詢.py
@@ -10,7 +10,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.common.keys import Keys
 
 import pandas as pd
 import time, os
