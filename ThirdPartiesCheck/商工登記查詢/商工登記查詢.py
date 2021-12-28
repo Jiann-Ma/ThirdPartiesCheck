369a790d68a2c8fda03aa6c31eb95a1891a42812
Jiann <72090089+Jiann-Ma@users.noreply.github.com>
2021-12-28 17:27:40 +0800

    Update 商工登記查詢.py
    修改掉截圖就會break的問題。

--- ThirdPartiesCheck/商工登記查詢/商工登記查詢.py
+++ ThirdPartiesCheck/商工登記查詢/商工登記查詢.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Dec  7 09:09:08 2021
-
 @author: ESB20914
 """
 
 # selenium libraries
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 import pandas as pd
 import time, os
@@ -59,23 +56,21 @@ def main(input_path, File_Path):
     build_directory()
 
     # 寫入所需資料
+    URL = 'https://findbiz.nat.gov.tw/fts/query/QueryBar/queryInit.do'
+    driver.get(URL)
+    # 點擊「分公司」
+    time.sleep(2)
+    driver.find_element(By.XPATH, '(//input[@name="qryType"])[2]').click()
+   
     for row in df_input.index:
-        
-        URL = 'https://findbiz.nat.gov.tw/fts/query/QueryBar/queryInit.do'
-        driver.get(URL)
-        time.sleep(2)
         forVerify = df_input.loc[row, 'companies']
+        
         # 加上編號
         forVerifyNo = f'{row}' + ". " + forVerify
-        # 點擊「分公司」
-        needsCheck = WebDriverWait(driver, 2).until(
-            EC.visibility_of_element_located((By.XPATH, '(//input[@name="qryType"])[2]')))
-        needsCheck.click()
         
         # 輸入要檢查的項目
         driver.find_element(By.XPATH, '//*[@id="qryCond"]').send_keys(forVerify)
-    
-        time.sleep(2)
+        
         # 按下送出查詢
         driver.find_element(By.XPATH, '//*[@id="qryBtn"]').click()
         time.sleep(2)
@@ -85,7 +80,9 @@ def main(input_path, File_Path):
             driver.find_element(By.XPATH, '//*[@id="vParagraph"]/div[1]/div[1]/a').click()
         except:
             driver.save_screenshot(File_Path + '\\' + f'{forVerifyNo}.png')
-            print("[INFO] 無查詢結果，拍照留存！") 
+            print("[INFO] 無查詢結果，拍照留存！")
+            driver.find_element(By.XPATH, '//*[@id="qryCond"]').clear()
+            continue
         time.sleep(2)
         
         # 點選法人董監網路試用版 (要切換frame)
@@ -93,9 +90,12 @@ def main(input_path, File_Path):
             iframe = driver.find_element(By.XPATH, '//*[@id="tabAdvanced"]') 
             iframe.click()
         except:
-           driver.switchTo().alert().accept()
-           iframe = driver.find_element(By.XPATH, '//*[@id="tabAdvanced"]') 
-           iframe.click()
+           driver.save_screenshot(File_Path + '\\' + f'{forVerifyNo}.png')
+           print("[INFO] 無查詢結果，拍照留存！") 
+           driver.find_element(By.XPATH, '//*[@id="bs-example-navbar-collapse-1"]/ul/li[3]').click()
+           time.sleep(2)
+           driver.find_element(By.XPATH, '//*[@id="qryCond"]').clear()
+           continue
         time.sleep(2)
         
         # 點選展開全部
@@ -108,20 +108,21 @@ def main(input_path, File_Path):
         # 找到所有有關係的公司
         nodes = driver.find_elements(By.XPATH, '//*[@id="chart"]')
         time.sleep(2)
+        
         # https://ithelp.ithome.com.tw/articles/10202725
         for i in range(len(nodes)):
-            # 如果沒資料，則txt檔案中顯示「無資料」
-            if nodes[i].text is None:
-                print("無資料")
-                with open(File_Path + '\\' + f"{forVerifyNo}.txt","w") as f:
-                    f.write("無資料")
-            else:
-                print(nodes[i].text)
-                with open(File_Path + '\\' + f"{forVerifyNo}.txt","w") as f:
-                    f.write(nodes[i].text)
+            print(nodes[i].text)
+            with open(File_Path + '\\' + f"{forVerifyNo}.txt","w") as f:
+                f.write(nodes[i].text)
         print(f'[INFO] 現在做完第{row}筆')
         time.sleep(1)
         
+        # 點擊「回查詢清單」
+        driver.switch_to.default_content()
+        driver.find_element(By.XPATH, '//*[@id="bs-example-navbar-collapse-1"]/ul/li[3]').click()
+        time.sleep(2)
+        driver.find_element(By.XPATH, '//*[@id="qryCond"]').clear()
+     
     print(f"[INFO] 總共{len(df_input)}筆，全部完成了！")
     driver.close()
 
